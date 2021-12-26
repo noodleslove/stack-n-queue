d1b960c91a06f57bc6f300b276c9eaba69c8d953
Edwin Ho <noodleslove@icloud.com>
2021-12-25 22:36:13 -0800

    PASS: added and tested stack

---
+++ stack/stack.go
@@ -0,0 +1,99 @@
+package stack
+
+import (
+	. "fake.com/tutorial/node"
+)
+
+type Stack struct {
+	top  *Node
+	size uint32
+}
+
+func NewStack() *Stack {
+	p := Stack{top: nil, size: 0}
+	return &p
+}
+
+/**
+ * Postcondition: The return value is true if stack object has no element,
+ *      else return false.
+ *
+ */
+func (s *Stack) Empty() bool {
+	return s.size == 0
+}
+
+/**
+ * Postcondition: The return value is the number of elements in the stack.
+ *
+ */
+func (s *Stack) Size() uint32 {
+	return s.size
+}
+
+/**
+ * Precondition: size() > 0
+ * Postcondition: The return value is the front item of the stack (but this
+ *      item is not removed from the queue).
+ *
+ */
+func (s *Stack) Top() interface{} {
+	if s.Empty() {
+		panic("Can't Top empty stack")
+	}
+
+	return s.top.Elem
+}
+
+/**
+ * Postcondition: The return value is an iterator to the top of the stack.
+ *
+ */
+func (s *Stack) Begin() *Node {
+	if s.Empty() {
+		return nil
+	}
+
+	return s.top
+}
+
+/**
+ * Postcondition: The return value is an iterator to nullptr.
+ *
+ */
+func (s *Stack) End() *Node {
+	return nil
+}
+
+/**
+ * Postcondition: Print all the elements in the queue to standard output.
+ *
+ */
+func (s *Stack) Display() {
+	PrintList(s.top)
+}
+
+/**
+ * Postcondition: A new element has been inserted at the top of the stack.
+ *
+ * @param item The item to be inserted.
+ */
+func (s *Stack) Push(elem interface{}) {
+	s.size++
+	InsertHead(&s.top, elem)
+}
+
+/**
+ * Precondition: size() > 0
+ * Postcondition: The top item of the stack has been removed. The return value
+ *      is the _item value of the removed node.
+ *
+ */
+func (s *Stack) Pop() interface{} {
+	if s.Empty() {
+		panic("Can't Pop empty stack")
+	}
+
+	s.size--
+	return DeleteNode(&s.top, s.top)
+}
---
+++ tests/stack_test.go
@@ -0,0 +1,47 @@
+package tests
+
+import (
+	. "fake.com/tutorial/stack"
+	"testing"
+)
+
+func TestStackConstructor(t *testing.T) {
+	s := NewStack()
+	if s == nil || !s.Empty() {
+		t.Errorf("NewStack incorrect")
+	}
+}
+
+func TestStackPush(t *testing.T) {
+	s := NewStack() // init Stack s
+	for i := 0; i < 1000; i++ {
+		s.Push(i) // push 0..999 to s
+	}
+
+	if s.Size() != 1000 { // test if stack size is correct
+		t.Errorf("Push incorrect, got: %d, want: %d", s.Size(), 1000)
+	}
+
+	// test if stack elements are correct
+	for i, x := s.Begin(), 999; i != s.End(); i, x = i.Next(), x-1 {
+		if i.Elem != x {
+			t.Errorf("Push incorrect, got: %d, want: %d", i.Elem, x)
+		}
+	}
+}
+
+func TestStackPop(t *testing.T) {
+	s := NewStack() // init Stack s
+	for i := 0; i < 1000; i++ {
+		s.Push(i) // push 0..999 to s
+	}
+
+	i := 999
+	for !s.Empty() {
+		val := s.Pop()
+		if val != i {
+			t.Errorf("Pop incorrect, got: %d, want: %d", val, i)
+		}
+		i--
+	}
+}
